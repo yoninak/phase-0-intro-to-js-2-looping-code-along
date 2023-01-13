af69f38255660f1edd283230187e91a730db21de
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-01-12 21:41:31 -0500

    commit tree 5f90ea986c7c3ae5daa90249be8103d36cb780de

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+
+function writeCards(arrayOfNames, wedding){
+    let container = []
+    for (let i = 0; i < arrayOfNames.length; i++){
+       container.push( `Thank you, ${arrayOfNames[i]}, for the wonderful ${wedding} gift!`)
+    }
+     return container
+}
+
+function countDown(startingNumber){
+    while (startingNumber >= 0){
+        console.log(startingNumber) 
+        startingNumber--
+    }
+}
+
